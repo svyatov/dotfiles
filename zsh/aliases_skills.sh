a5b0ed346d68a5ec663f508e301fd501335539cb
Leonid Svyatov <leonid@svyatov.com>
2026-03-20 14:01:39 +0300

    chore(zsh): rename gmdel to gmrm and add skills aliases
    Rename gem uninstall alias for consistency. Add aliases_skills.sh
    with shortcuts for npx skills commands (ska, skrm, sku, skl, skf, skc).

---
+++ zsh/aliases_skills.sh
@@ -0,0 +1,7 @@
+# Skills (npx skills)
+safe_alias ska 'npx skills add'
+safe_alias skrm 'npx skills remove'
+safe_alias sku 'npx skills update'
+safe_alias skl 'npx skills list'
+safe_alias skf 'npx skills find'
+safe_alias skc 'npx skills check'
